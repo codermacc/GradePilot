08c1f511bc034753ad01e656124a4e8007771ff6
codermacc <145511151+codermacc@users.noreply.github.com>
2026-06-18 00:56:09 +0100

    Import CSS files and initialize app on DOMContentLoaded

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+import "./styles/variables.css";
+import "./styles/themes.css";
+import "./styles/global.css";
+import "./styles/animations.css";
 
+import { initApp } from "./core/app.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initApp();
+});
